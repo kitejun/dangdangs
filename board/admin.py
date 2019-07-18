021f9e8b48a622e5c2e8fd7a086933c79af4db6b
Mox <kimyj7756@gmail.com>
2019-07-18 20:52:47 +0900

    초기화

--- board/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Board, Comment
-
-class BoardAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title')
-
-admin.site.register(Board, BoardAdmin)
-admin.site.register(Comment)
--- cal/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from cal.models import Event
-# Register your models here.
-
-admin.site.register(Event)
