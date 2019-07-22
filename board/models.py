4e2d5a11e96de3f82b10ea7d347c6c50aaf5eb4f
Mox <kimyj7756@gmail.com>
2019-07-22 22:21:01 +0900

    d

--- board/models.py
+++ board/models.py
@@ -5,10 +5,11 @@ from imagekit.processors import ResizeToFill
 # Create your models here.
 
 class Post(models.Model):
+    user_id = models.CharField(max_length=20)
     title=models.CharField(max_length=100)
     context=models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True) # 최초의 저장된 시간이 저장
-    updated_at = models.DateTimeField(auto_now=True) # 매번 저장이 될 때마다 시간이 저장
+    created_at = models.DateTimeField(null=True, blank=True) # 최초의 저장된 시간이 저장
+    updated_at = models.DateTimeField(null=True, blank=True) # 매번 저장이 될 때마다 시간이 저장
     image = models.ImageField(upload_to="images/%Y/%m/%d", default='https://image.flaticon.com/icons/svg/149/149852.svg')
     
     def __str__(self):
@@ -18,7 +19,7 @@ class Post(models.Model):
         return self.context[:50]
 
 class Comment(models.Model):
-    title = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.CharField(max_length=10)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
