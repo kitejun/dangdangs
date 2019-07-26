51bf98e9670b9388b67e35b5a2b5f18e22021073
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2019-07-26 20:58:14 +0900

    final heroku

--- dangdangs/settings.py
+++ dangdangs/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
