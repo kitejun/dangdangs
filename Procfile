a84e429698719fbd202962ad694565892352f5a4
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2019-07-26 20:47:52 +0900

    for heroku2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dangdangs.wsgi --log-file -
--- dangdangs/settings.py
+++ dangdangs/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +125,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
