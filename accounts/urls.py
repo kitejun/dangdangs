c50d5864dd3fba49215bed6ebedc700e54dd043f
Mox <kimyj7756@gmail.com>
2019-07-30 21:34:27 +0900

    초기화

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-  
-urlpatterns = [
-        path('signup/', views.signup, name='signup'),
-        path('login/', views.login, name='login'),
-        path('logout/', views.logout, name='logout'),    
-        path('mypage/', views.mypage, name='mypage')
-]
