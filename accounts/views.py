adaa7a2edf55f75bfc1247b76de7ffb1061ed607
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2019-08-01 16:52:37 +0900

    그래도 많이함

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib.auth import get_user_model
+from django.contrib import messages
+
 User = get_user_model()
 from .models import Group
 import random
@@ -13,12 +15,29 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
              
             user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'],groupid=request.POST['groupid'])
+            
+            ba = request.POST.get("groupjudge", None)
+            if ba is None: # 널이면 즉 체크박스에 체크가 안되어있으면
+                ra = random.randint(1,1000000001)
+                group = Group()
+                group.groupid= ra
+                group.save()
+            else:   #만약 가족코드를 입력했다면 
+                event = Group.objects.filter(groupid=user.groupid).first()
+                if event:
+                    return redirect('home')
+                else:
+                    return render(request, 'accounts/signup.html')
+                
 
-            group = Group()
-            group.groupid= user.groupid
-            group.save()
-            auth.login(request, user)
+
+
             return redirect('home')
+       
+        messages.warning(request,'비밀번호가 일치하지 않습니다.')
+
+
+
     return render(request, 'accounts/signup.html')
 
 
