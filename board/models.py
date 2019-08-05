6c7db7708f56f945280fa5fe58999487a728bcd8
rye0824 <fbduddms321@naver.com>
2019-08-06 05:45:05 +0900

    디자인 수정
    게시판 디자인수정(디테일,뉴,보드)

--- board/models.py
+++ board/models.py
@@ -9,12 +9,22 @@ class Board(models.Model):
     image = models.FileField(upload_to="images/%Y/%m/%d", default='https://image.flaticon.com/icons/svg/149/149852.svg')
     pub_date=models.DateTimeField('date published')
     context=models.TextField()
+    hits=models.PositiveIntegerField(default=0)
     
     def __str__(self):
         return self.title
 
     def summary(self):
         return self.context[:100]
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('detail', args=[self.id])
+
+    @property
+    def update_counter(self):
+        self.hits=self.hits+1
+        self.save()
 
 class Comment(models.Model):
     # author = models.ForeignKey(Board, settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
