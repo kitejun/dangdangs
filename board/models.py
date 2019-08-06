4dd0847548a92c932826910c1cd69026ed474c5c
LINKER <temp2temp@daum.net>
2019-08-06 21:19:40 +0900

    게시판 덧글수정
    수정이요!

--- board/models.py
+++ board/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.conf import settings # 외래키를 위한 라이브러리
 
 # Create your models here.
 
 class Board(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title=models.TextField()
     image = models.FileField(upload_to="images/%Y/%m/%d", default='https://image.flaticon.com/icons/svg/149/149852.svg')
     pub_date=models.DateTimeField('date published')
     context=models.TextField()
+    hits=models.PositiveIntegerField(default=0)
     
     def __str__(self):
         return self.title
@@ -14,7 +17,17 @@ class Board(models.Model):
     def summary(self):
         return self.context[:100]
 
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('detail', args=[self.id])
+
+    @property
+    def update_counter(self):
+        self.hits=self.hits+1
+        self.save()
+
 class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, null=True, related_name='comments')   
     comment_date = models.DateTimeField(auto_now_add=True)
     comment_body = models.CharField(max_length=50)
--- cal/views.py
+++ cal/views.py
@@ -95,11 +95,29 @@ def total(request):
 # 저장된 daily 값들을 불러오는 함수
 def daily(request): 
 
-    daily = Daily.objects.filter(date=timezone.now()).first() # 현재 날짜와 같은 오브젝트
+    daily = Daily.objects.filter(date=timezone.now(), groupid=request.user.groupid).first() # 현재 날짜와 같은 오브젝트
     if daily:
         instance = daily
-    else:
+    else: # 해당 오브젝트가 없으면 생성
         instance = Daily()
+        instance.groupid = request.user.groupid
+        instance.save()
+
+    form = DailyForm(request.POST or None, instance=instance)
+
+    return form
+
+# 저장된 daily 값들을 불러오는 함수
+def daily(request): 
+
+    daily = Daily.objects.filter(date=timezone.now(), groupid=request.user.groupid).first() # 현재 날짜와 같은 오브젝트
+    if daily:
+        instance = daily
+    else: # 해당 오브젝트가 없으면 생성
+        instance = Daily()
+        instance.groupid = request.user.groupid
+        instance.save()
+
     form = DailyForm(request.POST or None, instance=instance)
 
     return form
@@ -107,14 +125,11 @@ def daily(request):
 # daily를 저장하는 함수
 def daily_count(request):
     
-    daily = Daily.objects.filter(date=timezone.now()).first() # 현재 날짜와 같은 오브젝트
-    if daily:
-        instance = daily
-    else:
-        instance = Daily()
-    form = DailyForm(request.POST or None, instance=instance)
+    daily = Daily.objects.filter(date=timezone.now(), groupid=request.user.groupid).first() # 현재 날짜와 같은 오브젝트
+
+    form = DailyForm(request.POST or None, instance=daily)
     
-    if request.POST and form.is_valid():
+    if request.POST and form.is_valid(): # 제출시 form 내용 DB에 저장
         daily = form.save(commit=False)
         daily.groupid = request.user.groupid 
         daily.save()
